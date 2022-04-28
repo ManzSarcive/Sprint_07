a4dbfe1aa8848b375babf9a68b8920fc85885a59
Nanjiro <armougom.t@gmail.com>
2022-04-28 15:24:31 +0400

    WIP

--- resources/views/vendor/forum/category/index.blade.php
+++ resources/views/vendor/forum/category/index.blade.php
@@ -5,15 +5,13 @@
     <div class="d-flex flex-row justify-content-between mb-2">
         <h2 class="flex-grow-1">{{ trans('forum::general.index') }}</h2>
 
-        CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-
-        {{-- @can ('createCategories') --}}
+        @can ('createCategories')
             <button type="button" class="btn btn-primary" data-open-modal="create-category">
                 {{ trans('forum::categories.create') }}
             </button>
 
             @include ('forum::category.modals.create')
-        {{-- @endcan --}}
+        @endcan
     </div>
 
     @foreach ($categories as $category)
